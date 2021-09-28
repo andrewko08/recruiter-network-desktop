f9cc40250094066eb1c58c5238c10e7e5194b8f8
andrewko08 <ko.andrew.08@gmail.com>
2021-09-28 17:14:39 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<!-- 
+
+    NAVBAR SECTION 
+
+-->
+<div class="nav-container" tabindex="0">
+    <div class="nav-toggle"></div>
+
+    <nav class="nav-items">
+        <p class="nav-items-header">MAIN</p>
+
+        <div class="flex-row">
+            <embed src="../img/dashboard.svg">
+            <a class="nav-item" href="#">Dashboard</a>
+            <embed src="../img/lock.svg" id="lock-logo">
+        </div>
+        <div class="flex-row">
+            <embed src="../img/publishing-cropped.svg">
+            <a class="nav-item" href="#">Jobs</a>
+        </div>
+        <div class="flex-row">
+            <embed src="../img/talentpool.svg">
+            <a class="nav-item" href="#">Talentpool</a>
+        </div>
+        <div class="flex-row">
+            <embed src="../img/invoice.svg">
+            <a class="nav-item" href="#">Invoice</a>
+        </div>
+        <p class="nav-items-header">COMMUNICATIONS</p>
+        <div class="flex-row">
+            <embed src="../img/inbox.svg">
+            <a class="nav-item" href="#">Inbox</a>
+        </div>
+        <div class="flex-row">
+            <embed src="../img/chat.svg">
+            <a class="nav-item" href="#">Chat</a>
+            <embed src="../img/lock.svg" id="lock-logo">
+        </div>
+        <p class="nav-items-header">MANAGEMENT</p>
+        <div class="flex-row">
+            <embed src="../img/publishing-cropped.svg">
+            <a class="nav-item" href="#">Publishing</a>
+            <embed src="../img/lock.svg" id="lock-logo">
+        </div>
+        <div class="flex-row">
+            <embed src="../img/flexbase.svg">
+            <a class="nav-item" href="#">Flexbase</a>
+            <embed src="../img/lock.svg" id="lock-logo">
+        </div>
+        <div class="flex-row">
+            <embed src="../img/assessments.svg">
+            <a class="nav-item" href="#">Assessments</a>
+            <embed src="../img/lock.svg" id="lock-logo">
+        </div>
+    </nav>
+
+    <div class="nav-right-side-container">
+        <div class="nav-logo-title">
+            <embed src="../img/logo.svg">
+            <p>Recruiter Network.</p>
+        </div>
+
+        <div class="nav-settings-profile">
+            <embed src="../img/notif.svg">
+            <embed src="../img/notif-sign.svg" id="notif-sign-icon">
+            <p>NL</p>
+            <embed src="../img/up-line.svg" id="seperator">
+            <p>EN</p>
+            <div class="profile-picture"><img src="../img/face.png"></img></div>
+            <p id="black-font">Andrei</p>
+            <embed src="../img/down-triangle.svg">
+
+        </div>
+    </div>
+</div>
+
+<!-- 
+
+    PAGE CONTENTS SECTION
+
+ -->
+<div class="contents-wrapper">
+    <div class="four-buttons-container">
+        <div class="download btn">
+            <embed src="../img/cloud.svg">
+            <p>Download Resume</p>
+        </div>
+        <div class="reject btn">
+            <embed src="../img/reject.svg">
+            <p>Reject Candidate</p>
+        </div>
+        <div class="plan btn">
+            <embed src="../img/eye.svg">
+            <p>Plan Interview</p>
+        </div>
+        <div class="hire btn">
+            <embed src="../img/calendar.svg">
+            <p>Hire Candidate</p>
+        </div>
+    </div>
+    <div class="grid-1-3">
+        <div class="profile-card border-curved padded">
+            <img src="../img/profile-pic.png">
+
+            <div id="top-right">
+                <embed src="../img/edit.svg" id="edit">
+            </div>
+            <h2>Evan Yates</h2>
+            <p>UI/UX Designer</p>
+            <div class="location-container">
+                <embed src="../img/location.svg">
+                <p>NYC, New York, USA</p>
+            </div>
+            <span class="divider"></span>
+            <div class="three-buttons-container">
+                <div class="blue-button">
+                    <embed src="../img/Vector.svg">
+                    <p>5</p>
+                    <p>(4)</p>
+                </div>
+                <div class="yellow-button">
+                    <embed src="../img/carbon_alarm-add.svg">
+                    <p>80%</p>
+                    <p>(24)</p>
+                </div>
+                <div class="orange-button">
+                    <embed src="../img/db.svg">
+                    <p>$4 / hour</p>
+                </div>
+            </div>
+
+        </div>
+
+        <!-- SUMMARY CARD -->
+
+        <div class="summary-card border-curved padded">
+            <h2>Summary</h2>
+            <p>Amet, dignissim quam magna molestie et vitae eu nunc molestie. Ut massa est vel ac tempus id. Id consequat eros duis imperdiet. Pellentesque sed elit interdum urna commodo ultricies nisi convallis. Nisl, et, fermentum, nullam turpis risus sit auctor elit proin. Egestas pellentesque lectus ac non orci cras nulla tincidunt. Consequat, urna quisque dolor in diam, massa diam. Lectus id suscipit aenean arcu. Dolor condimentum in adipiscing vulputate pharetra mus scelerisque. Cras at dis eu bibendum. Integer vel pellentesque sem proin. Molestie habitasse lorem quis libero. Nisi malesuada nam lorem vel posuere. Dignissim mi, a mattis sit risus posuere morbi. Purus risus sit in rhoncus.
+                Orci, gravida dictum elit aliquet. Condimentum convallis ut mattis consectetur. Ultrices sed tempor sagittis, ornare. Nunc magna elementum nisl lectus volutpat sit phasellus viverra. Adipiscing ut lacus sodales enim, non phasellus tincidunt. Nisi pellentesque in nunc gravida aliquet amet, nec. Congue interdum mi nulla elit, convallis. Dui, sollicitudin sit fusce enim ultrices.
+            <br><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue elementum non molestie et vitae rutrum nunc. Orci, nam dolor urna vitae duis in non egestas. Amet, aliquet sed egestas lobortis nulla nisi, duis purus malesuada. Duis enim morbi amet, vulputate id gravida aliquet. Amet, dignissim quam magna molestie et vitae eu nunc molestie. Amet, dignissim quam magna molestie et vitae eu nunc molestie. Ut massa est vel ac tempus id. Id consequat eros duis imperdiet. Pellentesque sed elit interdum urna commodo ultricies nisi convallis. 
+            <br><br>
+            nullam turpis risus sit auctor elit proin. Egestas pellentesque lectus ac non orci cras nulla tincidunt. Consequat, urna quisque dolor in diam, massa diam. Lectus id suscipit aenean arcu. Dolor condimentum in adipiscing vulputate pharetra mus scelerisque. </p>
+        </div>
+    </div>
+
+
+    <!-- HIRING INFORMATION GRID SECTION -->
+
+    <div class="grid-2-1">
+    <div class="hiring-info-wrapper">
+            <h2>Hiring Information</h2>
+            <div class="hiring-info-card border-curved">
+                <div class="hiring-info-row-1">
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Salary Range</h3>
+                        </div>
+                        <div class="form-entry-content"><p>Evan Yates</p></div>
+                    </div>
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Moment of availability</h3>
+                        </div>
+                        <div class="form-entry-content"><p>Evan Yates</p></div>
+                    </div>
+                </div>
+                <div class="hiring-info-row-1">
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Hours per week/partime or fultime</h3>
+                        </div>
+                        <div class="form-entry-content"><p>40-40</p></div>
+                    </div>
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Industries</h3>
+                        </div>
+                        <div class="form-entry-content"><p>Yes.(2000-2004)</p></div>
+                    </div>
+                </div>
+                <div class="hiring-info-row-1">
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Transport option</h3>
+                        </div>
+                        <div class="form-entry-content"><p>Public Transport</p></div>
+                    </div>
+                    <div class="form-entry-container">
+                        <div class="form-entry-title">
+                            <embed src="../img/Oval.svg">
+                            <h3>Wants to be hired</h3>
+                        </div>
+                        <div class="form-entry-content"><p>Public Transport</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="hiring-info-card-2 border-curved">
+            <div class="hiring-info-wrapper">
+            <h2 id="hide">Hiring Information</h2>
+            <div class="hiring-info-card border-curved">
+                <div class="flex-row">
+                    <div class="grow">
+                        <div class="hiring-info-row-1">
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Experience</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Managment</p>
+                                    <p>Planning</p>
+                                    <p>Inside sales</p>
+                                    <p>Marketing</p>
+
+                                </div>
+                            </div>
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Years</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>3</p>
+                                    <p>4</p>
+                                    <p>5</p>
+                                    <p>5</p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="hiring-info-row-1">
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Education</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>HBO Management</p>
+                                    <p>IT management</p>
+                                </div>
+                            </div>
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Afgerond</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Yes</p>
+                                    <p>No</p>
+                                    <p>No</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="grow">
+                        <div class="hiring-info-row-1">
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Skills</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Python</p>
+                                    <p>SQL</p>
+                                    <p>Excel</p>
+                                    <p>Autocad</p>
+                                </div>
+                            </div>
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>level</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Professional</p>
+                                    <p>Intermediate</p>
+                                    <p>Expert</p>
+                                    <p>Professional</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="hiring-info-row-1">
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Industry</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Communicate</p>
+                                    <p>Marketing</p>
+                                    <p>Consultation</p>
+                                </div>
+                            </div>
+                            <div class="form-entry-container">
+                                <div class="form-entry-title">
+                                    <embed src="../img/Oval.svg">
+                                    <h3>Job category</h3>
+                                </div>
+                                <div class="form-entry-content">
+                                    <p>Administratie</p>
+                                    <p>Design</p>
+                                    <p>Accountmanager</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>     
+        </div>
+
+
+
+        <!-- WORK HISTORY SECTION -->
+
+        <div class="work-history-wrapper">
+            <h2>Work history</h2>
+            <div class="work-history-card border-curved">
+                <div class="history-column">
+                    <div class="history-row">
+                        <div class="history-content">
+                            <h3>2019-2020</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit duis nec nunc neque, nulla. Elementum id amet, est massa porttitor. Pharetra et, aliquam tellus est molestie orci, gravida adipiscing. Quisque quam sed sed facilisi mi aliquet in. Elit vitae elementum sit proin habitant vel. Malesuada massa purus rhoncus faucibus odio viverra. Ullamcorper in ultrices proin dolor donec</p>
+                        </div>
+                        <div class="history-content">
+                            <h3>2019-2020</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit duis nec nunc neque, nulla. Elementum id amet, est massa porttitor. Pharetra et, aliquam tellus est molestie orci, gravida adipiscing. Quisque quam sed sed facilisi mi aliquet in. Elit vitae elementum sit proin habitant</p>
+                        </div>
+                    </div>
+                    <div class="history-row">
+                        <div class="history-content">
+                            <h3>2019-2020</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit duis nec nunc neque, nulla. Elementum id amet, est massa porttitor. Pharetra et, aliquam tellus est molestie orci, gravida adipiscing. Quisque quam sed sed facilisi mi aliquet in. Elit vitae elementum sit proin habitant vel. Malesuada massa purus rhoncus faucibus odio viverra.</p>
+                        </div>
+                        <div class="history-content">
+                            <h3>2019-2020</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit duis nec nunc neque, nulla. Elementum id amet, est massa porttitor. Pharetra et, aliquam tellus est molestie orci, gravida adipiscing. Quisque quam sed sed facilisi mi aliquet in. Elit vitae elementum sit proin habitant vel. Malesuada massa purus rhoncus faucibus odio viverra. Ullamcorper in ultrices proin dolor donec nunc id. Lorem ipsum dolor sit amet,</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+<script>
+const nav = document.querySelector(".nav-container");
+
+if (nav) {
+  const toggle = nav.querySelector(".nav-toggle");
+  
+  if (toggle) {
+    toggle.addEventListener("click", () => {
+      if (nav.classList.contains("is-active")) {
+        nav.classList.remove("is-active");
+      }
+      else {
+        nav.classList.add("is-active");
+      }
+    });
+    
+    nav.addEventListener("blur", () => {
+      nav.classList.remove("is-active");
+    });
+  }
+}</script>
+</html>
